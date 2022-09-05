d287929ae6d444378185d85b18be9f70bea96a3b
bburton5 <bburton5@uw.edu>
2022-09-05 00:10:30 -0700

    README.md file is now created when all prompts are asked

--- Develop/index.js
+++ Develop/index.js
@@ -12,10 +12,10 @@ const questions = [
   {
     name: "description",
     type: "input",
-    message: "What is the desrciption of your project?",
+    message: "What is the description of your project?",
   },
   {
-    name: "installation-instructions",
+    name: "install",
     type: "input",
     message: "What are the installation instructions?",
   },
@@ -26,17 +26,17 @@ const questions = [
     choices: ["MIT", "ISC"],
   },
   {
-    name: "contribution-guidelines",
+    name: "contribution",
     type: "input",
     message: "What are the contribution guidelines?",
   },
   {
-    name: "test-instructions",
+    name: "test",
     type: "input",
     message: "What are the testing instructions?",
   },
   {
-    name: "github-handle",
+    name: "github",
     type: "input",
     message: "What is your github username?",
   },
@@ -48,18 +48,38 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile("README2.md", "this is the added README", function (err, data) {
+    console.log(Error);
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt([questions[0]]).then((answers) => {
+    console.log("Your Project's Title:", answers.title);
     inquirer.prompt([questions[1]]).then((answers) => {
+      console.log("Your Project's Description:", answers.description);
       inquirer.prompt([questions[2]]).then((answers) => {
+        console.log(
+          "Your Project's Installation Instructions:",
+          answers.install
+        );
         inquirer.prompt([questions[3]]).then((answers) => {
+          console.log("Your Project's Usage Info:", answers.usageinfo);
           inquirer.prompt([questions[4]]).then((answers) => {
+            console.log(
+              "Your Project's Contribution Guidelines:",
+              answers.constribution
+            );
             inquirer.prompt([questions[5]]).then((answers) => {
+              console.log("Your Project's Testing Instructions:", answers.test);
               inquirer.prompt([questions[6]]).then((answers) => {
-                inquirer.prompt([questions[7]]).then((answers) => {});
+                console.log("Your Github Username:", answers.github);
+                inquirer.prompt([questions[7]]).then((answers) => {
+                  console.log("Your Email Address:", answers.email);
+                  writeToFile();
+                });
               });
             });
           });
